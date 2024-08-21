769919b73a89858c311c3f9edc939c23f483d954
Errick Basa-Denis <errickbd@gmail.com>
2024-08-20 20:18:24 -0500

    modified db url

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = parseInt(process.env.PORT);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, 
